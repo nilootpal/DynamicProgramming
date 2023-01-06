94f991d54e5cd19162742a2a511243f91371feab
nilootpal <nilootpaldas@gmail.com>
2023-01-06 22:49:25 +0530

    Longest Increasing Subsequence

--- longestIncreasingSubsequence.cpp
+++ longestIncreasingSubsequence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -23,14 +24,29 @@ using namespace std;
 
 // Memoization
 
-int longestIncreasingSubsequence(vector<int>&nums){
-    return 0;
+map<int, int> memo;
+
+int longestIncreasingSubsequence(vector<int>&nums, int idx){
+    if(memo[idx]){
+        return memo[idx];
+    }
+
+    memo[idx] = 1;
+
+    for(int i = idx-1; i >= 0; --i){
+        if(nums[i] < nums[idx]){
+            int temp = longestIncreasingSubsequence(nums, i);
+            memo[idx] = max(memo[idx], 1 + temp);
+        }
+    }
+
+    return memo[idx];
 }
 
 int main(){
     vector<int> arr = {10, 22, 9, 33, 21, 50, 52, 60, 80}; 
     
-    cout<<longestIncreasingSubsequence(arr)<<endl;
+    cout<<longestIncreasingSubsequence(arr, 8)<<endl;
 
     return 0;
 }
